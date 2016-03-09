8a8ad3f648f156b7f4e0fc64e3fc8c15da71023f
Lestat Delionkur <lestatms@outlook.com>
2016-03-09 21:03:48 +0300

    Init

---
+++ Empl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Empl.Startup))]
+namespace Empl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
