ff8d6ba5ba73f42d86f14190b0eb28f0b2a2eb90
Lestat Delionkur <lestatms@outlook.com>
2016-03-11 00:18:36 +0300

    bd

--- Empl/Migrations/Configuration.cs
+++ Empl/Migrations/Configuration.cs
@@ -16,18 +16,13 @@ namespace Empl.Migrations
 
         protected override void Seed(Empl.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
             context.EmployeeTypes.AddOrUpdate(
               p => p.Title,
               new EmployeeType { Title = "Разработчик" },
               new EmployeeType { Title = "Тестировщик" },
               new EmployeeType { Title = "Менеджер" }
             );
-            //
+            
         }
     }
 }
